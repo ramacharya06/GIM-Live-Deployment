33132c77b95d193a10249565c7ee74301ccd91e9
Anas <86509653+AnasDharar@users.noreply.github.com>
2025-09-15 09:53:07 +0530

    UI improvement

--- gimlivedeploy/views.py
+++ gimlivedeploy/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Vote
+from .models import IPs
 
-
+def get_client_ip(request):
+    """Extract client IP address, even if behind proxy."""
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]  # first IP in list
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 def home(request):
 
@@ -11,7 +19,14 @@ def home(request):
 
 def submit_vote(request):
     if request.method == 'POST':
+        client_ip = get_client_ip(request)
+        print(f"Client IP: {client_ip}")
         votes = request.POST
+        if IPs.objects.filter(ip_address=client_ip).exists():
+            return render(request, 'already_voted.html')
+        else:
+            new_ip = IPs(ip_address=client_ip)
+            new_ip.save()
         # print(votes)
         for key, value in votes.items():
             if key == 'csrfmiddlewaretoken':
